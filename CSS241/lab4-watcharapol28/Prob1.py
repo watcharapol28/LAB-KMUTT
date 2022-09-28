111f21f83d7fc165c1a5be0af5bbec943a97448b
Watcahrapol <watcharapol.tree@kmutt.ac.th>
2022-09-28 23:07:11 +0700

    up

--- CSS241/lab4-watcharapol28/Prob1.py
+++ CSS241/lab4-watcharapol28/Prob1.py
@@ -4,16 +4,16 @@ import numpy as np
 def f(x):
     return x**6
 
-def psi(x, h):
+def diff(x, h):
     return (f(x + h) - f(x - h)) / (2*h)
 
 
-def richardson_diff(x, h=0.75, N=2):
-    t = np.zeros((N, N))
-    for i in range(N):
-        t[i][0] = psi(x, h)
-        for j in range(1, i+1):
+def richardson_diff(x, h = 0.75, N = 2):
+    t = np.zeros((N + 1, N + 1))
+    for i in range(N + 1):
+        t[i][0] = diff(x, h)
+        for j in range(1, i + 1):
             t[i][j] = t[i][j - 1] + ((t[i][j - 1] - t[i - 1][j - 1]) / (4**j - 1))
         h /= 2
-    return t[N-1][N-1]
+    return t[N][N]
 
